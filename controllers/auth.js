bfda5f4625fe58868312f60378277f9da69da5c1
notMaya0101 <franciajeremiah10@gmail.com>
2024-06-18 22:04:18 +0800

    add authentication js & cleaned up views and public

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,21 +2,8 @@ const User = require("../models/User");
 const { UnauthenticatedError, BadRequestError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 
-//const register = async (req, res) => {
-//  console.log("Starting registration...");
-//  try {
-//    const user = await User.create(req.body);
-//    console.log("User created successfully:", user);
-//    const token = user.createJWT();
-//    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
-//  } catch (error) {
-//    console.error(error);
-//    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err: error });
-//  }
-//};
-
 const register = async (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
   const user = await User.create(req.body);
   const token = user.createJWT;
   res.status(StatuesCodes.CREATED).json({ user: { name: user.name }, token });
@@ -28,9 +15,10 @@ const login = async (req, res) => {
   if (!email || !password) {
     throw new BadRequestError("Please provide email or password");
   }
-  const user = await User.findOne({ email });
 
   // check if user exists
+  const user = await User.findOne({ email });
+
   if (!user) {
     throw new UnauthenticatedError("Invalid credentials");
   }
@@ -44,35 +32,6 @@ const login = async (req, res) => {
   token = user.createJWT();
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
-
-//const login = async (req, res) => {
-//  try {
-//    const { email, password } = req.body;
-//
-//    if (!email || !password) {
-//      throw new BadRequestError("Please provide email or password");
-//    }
-//
-//    const user = await User.findOne({ email });
-//
-//    // check if user exists
-//    if (!user) {
-//      throw new UnauthenticatedError("Invalid credentials");
-//    }
-//
-//    // compare password
-//    const isPasswordCorrect = await user.comparePassword(password);
-//    if (!isPasswordCorrect) {
-//      throw new UnauthenticatedError("Invalid credentials");
-//    }
-//
-//    const token = user.createJWT();
-//    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
-//  } catch (err) {
-//    console.error(err);
-//    res.status(500).json({ error: "Internal Server Error" });
-//  }
-//};
 
 module.exports = {
   register,
