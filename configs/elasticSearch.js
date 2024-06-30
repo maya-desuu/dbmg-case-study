60a00aeb48f7cff6f71b0fe0eef691f248f80aeb
notMaya0101 <franciajeremiah10@gmail.com>
2024-07-01 07:49:37 +0800

    .

--- configs/elasticSearch.js
+++
@@ -1,9 +0,0 @@
-//const { Client } = require("@elastic/elasticsearch");
-//
-//const client = new Client({
-//  node: "http://localhost:9200",
-//});
-//
-//console.log("Elasticsearch Client Configuration:", client);
-//
-//module.exports = client;
